732ca73ee96f8d7ad1cb6c40ebe452577ccbfd75
Пашина Алина <nice.alina28@mail.ru>
2022-05-10 12:18:35 +0300

    new commit

--- Lab7_Tables/Lab7_Tables/TTabRecord.h
+++ Lab7_Tables/Lab7_Tables/TTabRecord.h
@@ -8,25 +8,29 @@ class TTabRecord;
 typedef TTabRecord* PTTabRecord;
 
 class TTabRecord : public TDatValue {
-protected:   
-    TKey Key;   
+protected:
+    TKey Key;
     PTDatValue pValue;
     virtual void Print()
     {
         std::cout << Key << " " << pValue->ToString() << std::endl;
     }
-public:  
+public:
     TTabRecord(TKey key, PTDatValue Val) : Key(key), pValue(Val) {};
     void SetKey(TKey k) { Key = k; }
-    TKey GetKey() { return Key; }  
+    TKey GetKey() { return Key; }
     void SetValue(PTDatValue p) { pValue = p; }
-    PTDatValue GetValue() { return pValue; } 
+    PTDatValue GetValue() { return pValue; }
     virtual TDatValue* GetCopy() { return new TTabRecord(Key, pValue); }
     TTabRecord& operator=(TTabRecord& tr) { Key = tr.Key; pValue = tr.pValue; return *this; }
     virtual bool operator==(const TTabRecord& tr) { return Key == tr.Key; }
     virtual bool operator<(const TTabRecord& tr) { return Key < tr.Key; }
     virtual bool operator>(const TTabRecord& tr) { return Key > tr.Key; }
-    std::string ToString() override { }
+    virtual std::string ToString() override {
+        // //    std::string res = this->GetKey() + "   " + (this->GetValue()->ToString());
+             //return nullptr;
+        return this->GetKey();
+    }
     friend class TArrayTable;
     friend class TScanTable;
     friend class TSortTable;
